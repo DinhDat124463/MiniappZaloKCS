085139469bfb2e582416c59c754d812699ab19b0
Đinh Đat <97022511+DinhDat124463@users.noreply.github.com>
2024-06-17 16:43:02 +0700

    a

--- lich-168/src/pages/detail_month.tsx
+++ lich-168/src/pages/detail_month.tsx
@@ -1,19 +1,14 @@
 import React, { useState } from "react";
-import { useTheme, Text, Page, Grid, Center, Box } from "zmp-ui";
-import { Col, Row, Button } from 'antd';
-import { project } from '../pages/add_Schedule';
+import { useTheme, Text, Grid } from "zmp-ui";
+import { Col, Row } from 'antd';
 import { tuan2 } from '../pages/calendar_month';
 const Detail: React.FC = () => {
   const [theme] = useTheme();
-  const [selectedRow, setSelectedRow] = useState<number | null>(null);
-  const [selectedColumn, setSelectedColumn] = useState<number | null>(null);
   const [data, setData] = useState<string[][]>([]);
-  const [data2, setData2] = useState<string[][]>([]);
-
   const renderRow = (rowIndex: number) => {
     return (
       <Grid >
-        <Row className="grid_column">
+        <Row className="grid_column grid_header">
           <Col className="grid_column" span={3}><Text>T2</Text></Col>
           <Col className="grid_column" span={3}><Text>T3</Text></Col>
           <Col className="grid_column" span={3}><Text>T4</Text></Col>
@@ -22,23 +17,84 @@ const Detail: React.FC = () => {
           <Col className="grid_column" span={3}><Text>T7</Text></Col>
           <Col className="grid_column" span={3}><Text>CN</Text></Col>
         </Row>
+
         <Row className="grid_column">
-          <Col className="grid_column" span={3}><Text>{tuan2.startDate}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[1]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[2]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[3]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[4]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[5]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data[6]}</Text></Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+
         </Row>
+
         <Row className="grid_column">
-          <Col className="grid_column" span={3}><Text>{data2[0]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data2[1]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data2[2]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data2[4]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data2[5]}</Text></Col>
-          <Col className="grid_column" span={3}><Text>{data2[6]}</Text></Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{"iuouioui564654654645oiuouio"}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+          <Col className="grid_column" span={3}>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+            <Row className="grid_column"><Text className="text">{tuan2.startDate}</Text></Row>
+          </Col>
+
         </Row>
       </Grid>
     );
